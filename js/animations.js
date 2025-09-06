35bbc53372b48e516a64cb20859f317845464c9e
Андрей Теряев <teryaev.2004@mail.ru>
2025-09-06 03:02:08 +0300

    main / added ease animations for hero block

---
+++ js/animations.js
@@ -0,0 +1,14 @@
+gsap.from(".hero__content", {
+  y: 70,
+  opacity: 0,
+  duration: 1.35,
+  delay: 0.8,
+  ease: "power2.out",
+});
+
+gsap.from(".header", {
+  y: -150,
+  opacity: 0,
+  duration: 1.35,
+  ease: "power2.out",
+});
