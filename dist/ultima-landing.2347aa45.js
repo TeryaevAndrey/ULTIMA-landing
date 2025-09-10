854e01b81bff94e664021f29bf4a863c88a713eb
Андрей Теряев <teryaev.2004@mail.ru>
2025-09-10 18:15:42 +0300

    fix

---
+++ dist/ultima-landing.2347aa45.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",()=>{let e=document.querySelector(".page-loader");e?setTimeout(()=>{e.style.opacity=0,e.style.zIndex=-1e3,startAnimations()},2e3):startAnimations()});
+//# sourceMappingURL=ultima-landing.2347aa45.js.map
